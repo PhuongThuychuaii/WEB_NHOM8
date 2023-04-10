217eda0d0605a1d01f2a283712b66e31852f0087
PhuongThuychuaii <130304548+PhuongThuychuaii@users.noreply.github.com>
2023-04-10 17:38:59 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// Xử lý tắt mở menu thanh điều hướng
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('header .navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+}
+
+
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+})
